4bd97b076bb8ea8cc15adb4a88f1424f7222d48a
Bartosz Butrym <bartoszbutrym@MacBook-Pro-Bartosz.local>
2023-12-19 08:57:30 +0100

    fix undefined window

--- src/scripts/sidebar.ts
+++ src/scripts/sidebar.ts
@@ -1,7 +1,9 @@
-window.addEventListener("DOMContentLoaded", () => {
-  const body = document.querySelector("body");
-  const sidebarHook = document.createElement("div");
-  sidebarHook.id = "sidebar-hook";
+if (typeof window !== "undefined") {
+  window.addEventListener("DOMContentLoaded", () => {
+    const body = document.querySelector("body");
+    const sidebarHook = document.createElement("div");
+    sidebarHook.id = "sidebar-hook";
 
-  if (body !== null) body.appendChild(sidebarHook);
-});
+    if (body !== null) body.appendChild(sidebarHook);
+  });
+}
